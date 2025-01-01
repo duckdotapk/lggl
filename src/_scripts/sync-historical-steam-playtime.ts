fbbc30e49bda3917db33c1a7facd7bdbeea64547
Loren Goodwin <18320375+duckdotapk@users.noreply.github.com>
2025-01-01 13:34:46 -0500

    refactor: massively improved sync-historical-steam-playtime

--- src/_scripts/sync-historical-steam-playtime.ts
+++ src/_scripts/sync-historical-steam-playtime.ts
@@ -9,8 +9,174 @@ import { prismaClient } from "../_shared/instances/prismaClient.js";
 import { DateTime } from "luxon";
 
 //
+// Schemas
+//
+
+const IPlayerServiceClientGetLastPlayedTimesV1ResponseSchema = z.object(
+	{
+		response: z.object(
+			{
+				games: z.array(z.object(
+					{
+						appid: z.number(),
+						last_playtime: z.number(),
+						playtime_forever: z.number(),
+						first_playtime: z.number(),
+						playtime_windows_forever: z.number(),
+						playtime_mac_forever: z.number(),
+						playtime_linux_forever: z.number(),
+						playtime_deck_forever: z.number(),
+						playtime_disconnected: z.number(),
+					})),
+			}),
+	});
+
+const IFamilyGroupsServiceGetFamilyGroupForUserV1ResponseSchema = z.object(
+	{
+		response: z.object(
+			{
+				family_groupid: z.string(),
+			}),
+	});
+
+const IFamilyGroupsServiceGetPlaytimeSummaryV1ResponseSchema = z.object(
+	{
+		response: z.object(
+			{
+				entries: z.array(z.object(
+					{
+						steamid: z.string(),
+						appid: z.number(),
+						first_played: z.number(),
+						latest_played: z.number(),
+						seconds_played: z.number(),
+					})),
+
+				entries_by_owner: z.array(z.object(
+					{
+						steamid: z.string(),
+						appid: z.number(),
+						first_played: z.number(),
+						latest_played: z.number(),
+						seconds_played: z.number(),
+					})),
+			}),
+	});
+
+//
+// Types
+//
+
+type IPlayerServiceClientGetLastPlayedTimesV1Response = z.infer<typeof IPlayerServiceClientGetLastPlayedTimesV1ResponseSchema>;
+
+type IFamilyGroupsServiceGetFamilyGroupForUserV1Response = z.infer<typeof IFamilyGroupsServiceGetFamilyGroupForUserV1ResponseSchema>;
+
+type IFamilyGroupsServiceGetPlaytimeSummaryV1Response = z.infer<typeof IFamilyGroupsServiceGetPlaytimeSummaryV1ResponseSchema>;
+
+//
 // Functions
 //
+
+type FetchSteamDataResult =
+{
+	playerLastPlayedTimes: IPlayerServiceClientGetLastPlayedTimesV1Response["response"];
+	familyGroupForUser: null;
+	familyGroupPlaytimeSummary: null;
+} |
+{
+	playerLastPlayedTimes: IPlayerServiceClientGetLastPlayedTimesV1Response["response"];
+	familyGroupForUser: IFamilyGroupsServiceGetFamilyGroupForUserV1Response["response"];
+	familyGroupPlaytimeSummary: IFamilyGroupsServiceGetPlaytimeSummaryV1Response["response"];
+};
+
+async function fetchSteamData(): Promise<FetchSteamDataResult>
+{
+	//
+	// Get Player Last Played Times
+	//
+
+	const playerLastPlayedTimesSearchParameters = new URLSearchParams();
+
+	playerLastPlayedTimesSearchParameters.set("key", configuration.steamApiKey);
+
+	const playerLastPlayedTimesResponse = await fetch("https://api.steampowered.com/IPlayerService/ClientGetLastPlayedTimes/v1?" + playerLastPlayedTimesSearchParameters.toString());
+
+	const playerLastPlayedTimesResponseParseResult = IPlayerServiceClientGetLastPlayedTimesV1ResponseSchema.safeParse(await playerLastPlayedTimesResponse.json());
+
+	if (!playerLastPlayedTimesResponseParseResult.success)
+	{
+		throw new Error("Failed to get client last played times: " + JSON.stringify(playerLastPlayedTimesResponseParseResult.error));
+	}
+
+	const playerLastPlayedTimes = playerLastPlayedTimesResponseParseResult.data.response;
+	
+	//
+	// Get Family Group For User
+	//
+
+	const familyGroupForUserSearchParameters = new URLSearchParams();
+
+	familyGroupForUserSearchParameters.set("access_token", configuration.steamAccessToken);
+
+	familyGroupForUserSearchParameters.set("steamid", configuration.steamUserId);
+
+	const familyGroupForUserResponse = await fetch("https://api.steampowered.com/IFamilyGroupsService/GetFamilyGroupForUser/v1?" + familyGroupForUserSearchParameters.toString());
+
+	// TODO: make this work even if you're not in a family group, I don't think it will right now
+	const familyGroupForUserResponseParseResult = IFamilyGroupsServiceGetFamilyGroupForUserV1ResponseSchema.safeParse(await familyGroupForUserResponse.json());
+
+	if (!familyGroupForUserResponseParseResult.success)
+	{
+		return {
+			playerLastPlayedTimes,
+			familyGroupForUser: null,
+			familyGroupPlaytimeSummary: null,
+		};
+	}
+
+	const familyGroupId = parseInt(familyGroupForUserResponseParseResult.data.response.family_groupid);
+
+	if (isNaN(familyGroupId))
+	{
+		return {
+			playerLastPlayedTimes,
+			familyGroupForUser: null,
+			familyGroupPlaytimeSummary: null,
+		};
+	}
+
+	const familyGroupForUser = familyGroupForUserResponseParseResult.data.response;
+
+	//
+	// Get Family Group Playtime Summary
+	//
+
+	const familyGroupPlaytimeSummarySearchParameters = new URLSearchParams();
+
+	familyGroupPlaytimeSummarySearchParameters.set("access_token", configuration.steamAccessToken);
+
+	familyGroupPlaytimeSummarySearchParameters.set("family_groupid", familyGroupForUser.family_groupid);
+
+	const response = await fetch("https://api.steampowered.com/IFamilyGroupsService/GetPlaytimeSummary/v1?" + familyGroupPlaytimeSummarySearchParameters.toString(),
+		{
+			method: "POST",
+		});
+
+	const responseParseResult = IFamilyGroupsServiceGetPlaytimeSummaryV1ResponseSchema.safeParse(await response.json());
+
+	if (!responseParseResult.success)
+	{
+		throw new Error("Failed to get family group playtime summary: " + JSON.stringify(responseParseResult.error));
+	}
+
+	const familyGroupPlaytimeSummary = responseParseResult.data.response;
+
+	return {
+		playerLastPlayedTimes,
+		familyGroupForUser,
+		familyGroupPlaytimeSummary,
+	};
+}
 
 type PlayTime =
 {
@@ -24,139 +190,36 @@ type PlayTime =
 	unknownPlayTimeSeconds: number;
 };
 
-async function getPlayTimeFromClientLastPlayTimesEndpoint(steamAppId: number): Promise<PlayTime | null>
+function getPlayTime(steamData: FetchSteamDataResult, steamAppId: number): PlayTime | null
 {
-	const searchParameters = new URLSearchParams();
+	const ownedGame = steamData.playerLastPlayedTimes.games.find(game => game.appid === steamAppId);
 
-	searchParameters.set("key", configuration.steamApiKey);
-
-	const clientGetLastPlayedTimesResponse = await fetch("https://api.steampowered.com/IPlayerService/ClientGetLastPlayedTimes/v1?" + searchParameters.toString());
-
-	const clientGetLastPlayedTimesResponseParseResult = z.object(
-		{
-			response: z.object(
-				{
-					games: z.array(z.object(
-						{
-							appid: z.number(),
-							last_playtime: z.number(),
-							playtime_forever: z.number(),
-							first_playtime: z.number(),
-							playtime_windows_forever: z.number(),
-							playtime_mac_forever: z.number(),
-							playtime_linux_forever: z.number(),
-							playtime_deck_forever: z.number(),
-							playtime_disconnected: z.number(),
-						})),
-				}),
-		}).safeParse(await clientGetLastPlayedTimesResponse.json());
-
-	if (!clientGetLastPlayedTimesResponseParseResult.success)
+	if (ownedGame != null)
 	{
-		return null;
-	}
-
-	const ownedGame = clientGetLastPlayedTimesResponseParseResult.data.response.games.find(game => game.appid === steamAppId);
-
-	if (ownedGame == null)
-	{
-		return null;
-	}
-
-	return {
-		firstPlayedTimestamp: ownedGame.first_playtime,
-		lastPlayedTimestamp: ownedGame.last_playtime,
-
-		windowsPlayTimeSeconds: ownedGame.playtime_windows_forever * 60,
-		macPlayTimeSeconds: ownedGame.playtime_mac_forever * 60,
-		linuxPlayTimeSeconds: (ownedGame.playtime_linux_forever - ownedGame.playtime_deck_forever) * 60,
-		steamDeckPlayTimeSeconds: ownedGame.playtime_deck_forever * 60,
-		unknownPlayTimeSeconds:
-		(
-			// Note: playtime_disconnected is apparently NOT included in playtime_forever
+		return {
+			firstPlayedTimestamp: ownedGame.first_playtime,
+			lastPlayedTimestamp: ownedGame.last_playtime,
+	
+			windowsPlayTimeSeconds: ownedGame.playtime_windows_forever * 60,
+			macPlayTimeSeconds: ownedGame.playtime_mac_forever * 60,
+			linuxPlayTimeSeconds: (ownedGame.playtime_linux_forever - ownedGame.playtime_deck_forever) * 60,
+			steamDeckPlayTimeSeconds: ownedGame.playtime_deck_forever * 60,
+			unknownPlayTimeSeconds:
 			(
-				ownedGame.playtime_forever -
-				ownedGame.playtime_windows_forever -
-				ownedGame.playtime_mac_forever -
-				ownedGame.playtime_linux_forever -
-				ownedGame.playtime_deck_forever
-			) +
-			ownedGame.playtime_disconnected
-		) * 60,
-	};
-}
-
-async function getSteamFamilyGroupId(): Promise<number | null>
-{
-	const searchParameters = new URLSearchParams();
-
-	searchParameters.set("access_token", configuration.steamAccessToken);
-
-	searchParameters.set("steamid", configuration.steamUserId);
-
-	const response = await fetch("https://api.steampowered.com/IFamilyGroupsService/GetFamilyGroupForUser/v1?" + searchParameters.toString());
-
-	const responseParseResult = z.object(
-		{
-			response: z.object(
-				{
-					family_groupid: z.string(),
-				}),
-		}).safeParse(await response.json());
-
-	if (!responseParseResult.success)
-	{
-		return null;
+				// Note: playtime_disconnected is apparently NOT included in playtime_forever
+				//	playtime_deck_forever also should NOT be subtracted here, because it's included in playtime_linux_forever
+				(
+					ownedGame.playtime_forever -
+					ownedGame.playtime_windows_forever -
+					ownedGame.playtime_mac_forever -
+					ownedGame.playtime_linux_forever
+				) +
+				ownedGame.playtime_disconnected
+			) * 60,
+		};
 	}
 
-	const familyGroupId = parseInt(responseParseResult.data.response.family_groupid);
-
-	if (isNaN(familyGroupId))
-	{
-		return null;
-	}
-
-	return familyGroupId;
-}
-
-async function getPlayTimeFromGetPlaytimeSummaryEndpoint(steamAppId: number, steamFamilyGroupId: number): Promise<PlayTime | null>
-{
-	const searchParameters = new URLSearchParams();
-
-	searchParameters.set("access_token", configuration.steamAccessToken);
-
-	searchParameters.set("family_groupid", steamFamilyGroupId.toString());
-
-	const response = await fetch("https://api.steampowered.com/IFamilyGroupsService/GetPlaytimeSummary/v1?" + searchParameters.toString(),
-		{
-			method: "POST",
-		});
-
-	const responseParseResult = z.object(
-		{
-			response: z.object(
-				{
-					entries: z.array(z.object(
-						{
-							steamid: z.string(),
-							appid: z.number(),
-							first_played: z.number(),
-							latest_played: z.number(),
-							seconds_played: z.number(),
-						})),
-
-					entries_by_owner: z.array(z.object(
-						{
-							steamid: z.string(),
-							appid: z.number(),
-							first_played: z.number(),
-							latest_played: z.number(),
-							seconds_played: z.number(),
-						})),
-				}),
-		}).safeParse(await response.json());
-
-	if (!responseParseResult.success)
+	if (steamData.familyGroupForUser == null)
 	{
 		return null;
 	}
@@ -164,8 +227,8 @@ async function getPlayTimeFromGetPlaytimeSummaryEndpoint(steamAppId: number, ste
 	const entries =
 	[
 		// Note: I'm not sure if entries includes everything in entries_by_owner, so I'm including both just in case
-		...responseParseResult.data.response.entries,
-		...responseParseResult.data.response.entries_by_owner,
+		...steamData.familyGroupPlaytimeSummary.entries,
+		...steamData.familyGroupPlaytimeSummary.entries_by_owner,
 	];
 
 	for (const entry of entries)
@@ -190,33 +253,7 @@ async function getPlayTimeFromGetPlaytimeSummaryEndpoint(steamAppId: number, ste
 	return null;
 }
 
-async function getSteamPlayTime(steamAppId: number): Promise<PlayTime | null>
-{
-	const playTimeFromClientLastPlayTimesEndpoint = await getPlayTimeFromClientLastPlayTimesEndpoint(steamAppId);
-
-	if (playTimeFromClientLastPlayTimesEndpoint != null)
-	{
-		return playTimeFromClientLastPlayTimesEndpoint;
-	}
-
-	const steamFamilyGroupId = await getSteamFamilyGroupId();
-
-	if (steamFamilyGroupId == null)
-	{
-		return null;
-	}
-
-	const playTimeFromGetPlaytimeSummaryEndpoint = await getPlayTimeFromGetPlaytimeSummaryEndpoint(steamAppId, steamFamilyGroupId);
-
-	if (playTimeFromGetPlaytimeSummaryEndpoint != null)
-	{
-		return playTimeFromGetPlaytimeSummaryEndpoint;
-	}
-
-	return null;
-}
-
-async function getNonHistoricalPlaytimeForPlatform(gameId: number, platformId: number)
+async function getNonHistoricalPlaytimeForPlatform(game_id: number, platform_id: number)
 {
 	const aggregate = await prismaClient.gamePlayActionSession.aggregate(
 		{
@@ -230,22 +267,27 @@ async function getNonHistoricalPlaytimeForPlatform(gameId: number, platformId: n
 				
 				gamePlayAction:
 				{
-					game_id: gameId,
+					game_id: game_id,
 				},
-				platform_id: platformId,
+				platform_id: platform_id,
 			},
 		});
 
 	return aggregate._sum.playTimeSeconds ?? 0;
 }
 
-async function createHistoricalGameActionPlaySession(gamePlayActionId: number, platformId: number, playTimeSeconds: number)
+async function createHistoricalGameActionPlaySession(sortOrder: number, gamePlayAction_id: number, platform_id: number, playTimeSeconds: number)
 {
+	if (playTimeSeconds <= 0)
+	{
+		return;
+	}
+
 	await prismaClient.gamePlayActionSession.create(
 		{
 			data:
 			{
-				sortOrder: -1,
+				sortOrder,
 				
 				startDate: DateTime.fromSeconds(0).toJSDate(),
 				endDate: DateTime.fromSeconds(playTimeSeconds).toJSDate(),
@@ -254,14 +296,16 @@ async function createHistoricalGameActionPlaySession(gamePlayActionId: number, p
 				isHistorical: true,
 				notes: "Historical playtime from Steam.",
 
-				gamePlayAction_id: gamePlayActionId,
-				platform_id: platformId,
+				gamePlayAction_id: gamePlayAction_id,
+				platform_id: platform_id,
 			},
 		});
 }
 
 async function main()
 {
+	const steamData = await fetchSteamData();
+
 	const games = await prismaClient.game.findMany(
 		{
 			where:
@@ -287,7 +331,7 @@ async function main()
 			throw new Error("Game #" + game.id + " has no game play action!");
 		}
 
-		const playTime = await getSteamPlayTime(parseInt(game.steamAppId!));
+		const playTime = await getPlayTime(steamData, parseInt(game.steamAppId!));
 
 		if (playTime == null)
 		{
@@ -313,30 +357,15 @@ async function main()
 				},
 			});
 
-		if (playTime.windowsPlayTimeSeconds > 0)
-		{
-			await createHistoricalGameActionPlaySession(gamePlayAction.id, configuration.platformIds.windows, playTime.windowsPlayTimeSeconds - nonHistoricalWindowsPlayTime);
-		}
+		await createHistoricalGameActionPlaySession(-1, gamePlayAction.id, configuration.platformIds.windows, playTime.windowsPlayTimeSeconds - nonHistoricalWindowsPlayTime);
 
-		if (playTime.macPlayTimeSeconds > 0)
-		{
-			await createHistoricalGameActionPlaySession(gamePlayAction.id, configuration.platformIds.mac, playTime.macPlayTimeSeconds - nonHistoricalMacPlayTime);
-		}
+		await createHistoricalGameActionPlaySession(-2, gamePlayAction.id, configuration.platformIds.mac, playTime.macPlayTimeSeconds - nonHistoricalMacPlayTime);
 
-		if (playTime.linuxPlayTimeSeconds > 0)
-		{
-			await createHistoricalGameActionPlaySession(gamePlayAction.id, configuration.platformIds.linux, playTime.linuxPlayTimeSeconds - nonHistoricalLinuxPlayTime);
-		}
+		await createHistoricalGameActionPlaySession(-3, gamePlayAction.id, configuration.platformIds.linux, playTime.linuxPlayTimeSeconds - nonHistoricalLinuxPlayTime);
 
-		if (playTime.steamDeckPlayTimeSeconds > 0)
-		{
-			await createHistoricalGameActionPlaySession(gamePlayAction.id, configuration.platformIds.steamDeck, playTime.steamDeckPlayTimeSeconds - nonHistoricalSteamDeckPlayTime);
-		}
+		await createHistoricalGameActionPlaySession(-4, gamePlayAction.id, configuration.platformIds.steamDeck, playTime.steamDeckPlayTimeSeconds - nonHistoricalSteamDeckPlayTime);
 
-		if (playTime.unknownPlayTimeSeconds > 0)
-		{
-			await createHistoricalGameActionPlaySession(gamePlayAction.id, configuration.platformIds.unknown, playTime.unknownPlayTimeSeconds - nonHistoricalUnknownPlayTime);
-		}
+		await createHistoricalGameActionPlaySession(-5, gamePlayAction.id, configuration.platformIds.unknown, playTime.unknownPlayTimeSeconds - nonHistoricalUnknownPlayTime);
 
 		await prismaClient.gamePlayActionSession.updateMany(
 			{
