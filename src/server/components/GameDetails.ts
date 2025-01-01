66787748eb90898a6c2e8e3597c8b4a0eac38669
Loren Goodwin <18320375+duckdotapk@users.noreply.github.com>
2025-01-01 15:06:49 -0500

    feat: added more details to GameDetails

--- src/server/components/GameDetails.ts
+++ src/server/components/GameDetails.ts
@@ -12,6 +12,7 @@ import { Anchor } from "./Anchor.js";
 import { Button } from "./Button.js";
 import { GameFlagsToolbar } from "./GameFlagsToolbar.js";
 import { HumanDateTime } from "./HumanDateTime.js";
+import { Paragraph } from "./Paragraph.js";
 
 import { staticMiddleware } from "../instances/server.js";
 
@@ -21,11 +22,11 @@ import { configuration } from "../../_shared/libs/Configuration.js";
 // Locals
 //
 
-function Header(game: GameDetailsGame)
+function Banner(game: GameDetailsGame)
 {
 	return new DE("header",
 		{
-			class: "component-game-details-header",
+			class: "component-game-details-banner",
 		},
 		[
 			game.bannerImagePath != null
@@ -69,14 +70,14 @@ function PlayActionButtonGroup(game: GameDetailsGame)
 		]);
 }
 
-function Section(text: string, children: Child)
+function Section(children: Child)
 {
-	return new DE("section", "component-game-details-section",
-		[
-			new DE("header", "header", text),
+	return new DE("section", "component-game-details-section", children);
+}
 
-			children,
-		]);
+function Header(text: string)
+{
+	return new DE("header", "component-game-details-header", text);
 }
 
 type DataTableRow =
@@ -85,11 +86,13 @@ type DataTableRow =
 	value: Child;
 };
 
-function DataTable(rows: DataTableRow[])
+function DataTable(rows: (DataTableRow | null)[])
 {
 	return new DE("table", "component-game-details-data-table",
 		[
-			rows.map((row) => new DE("tr", null,
+			rows
+				.filter((row) => row != null)	
+				.map((row) => new DE("tr", null,
 				[
 					new DE("td", "label", row.label),
 
@@ -152,7 +155,7 @@ export function GameDetails(game: GameDetailsGame, recentGamePlayActionSessions:
 			"data-game-id": game.id,
 		},
 		[
-			Header(game),
+			Banner(game),
 
 			PlayActionButtonGroup(game),
 
@@ -160,10 +163,19 @@ export function GameDetails(game: GameDetailsGame, recentGamePlayActionSessions:
 
 			new DE("div", "details-sections",
 				[
-					Section("Details",
+					Section(
 						[
+							Header("Play data"),
+
 							DataTable(
 								[
+									game.completionStatus != null
+										? {
+											label: "Completion status",
+											value: game.completionStatus, // TODO: map enum value to a friendly string
+										}
+										: null,
+
 									{
 										label: "Time played",
 										value: game.playTimeTotalSeconds > 0
@@ -188,53 +200,108 @@ export function GameDetails(game: GameDetailsGame, recentGamePlayActionSessions:
 												])
 											: "Never played",
 									},
+									
+									game.firstPlayedDate != null
+										? {
+											label: game.firstPlayedDateApproximated ? "Year first played" : "Date first played",
+											value: game.firstPlayedDateApproximated
+												? DateTime.fromJSDate(game.firstPlayedDate).year
+												: HumanDateTime(DateTime.fromJSDate(game.firstPlayedDate), DateTime.DATE_MED),
+										}
+										: null,
+
+									game.firstCompletedDate != null
+										? {
+											label: game.firstCompletedDateApproximated ? "Year first completed" : "Date first completed",
+											value: game.firstCompletedDateApproximated
+												? DateTime.fromJSDate(game.firstCompletedDate).year
+												: HumanDateTime(DateTime.fromJSDate(game.firstCompletedDate), DateTime.DATE_MED),
+										}
+										: null,
+
+									game.lastPlayedDate != null
+										? {
+											label: "Date last played",
+											value: HumanDateTime(DateTime.fromJSDate(game.lastPlayedDate), DateTime.DATE_MED),
+										}
+										: null,
+								]), 
+						]),
+
+					Section(
+						[
+							Header("Recent play sessions"),
+
+							recentGamePlayActionSessions.length > 0
+								? DataTable(recentGamePlayActionSessions.map((gamePlayActionSession) =>
+									{
+										let title = "Played for " + Utilities.NumberLib.format(gamePlayActionSession.playTimeSeconds) + " seconds on " + gamePlayActionSession.platform.name;
+
+										if (gamePlayActionSession.notes != null)
+										{
+											title += "\n\n" + gamePlayActionSession.notes;
+										}
+
+										if (configuration.developerMode)
+										{
+											title += "\n\nGamePlayActionSession #" + gamePlayActionSession.id;
+										}
+
+										return {
+											label: !gamePlayActionSession.isHistorical
+												? HumanDateTime(DateTime.fromJSDate(gamePlayActionSession.startDate))
+												: "Historical",
+											value: new DE("span",
+												{
+													title,
+												},
+												[
+													new DE("span", gamePlayActionSession.platform.iconName + " fa-fw"),
+													" ",
+													humanizeDuration(gamePlayActionSession.playTimeSeconds * 1000, { units: [ "h", "m", "s" ] }),
+												]),
+										};
+									}))
+								: Paragraph(configuration.gamePlayActionSessionHistoryDays != -1
+									? "No play sessions in the last " + configuration.gamePlayActionSessionHistoryDays + " days."
+									: "No play sessions recorded."),
+						]),
+
+					Section(
+						[
+							Header("Metadata"),
+
+							DataTable(
+								[
+									{
+										label: "Progression type",
+										value: game.progressionType, // TODO: map enum value to a friendly string
+									},
 									{
 										label: "Steam app ID",
 										value: game.steamAppId != null
 											? game.steamAppId
 											: "-",
 									},
-								]), 
+								]),
 						]),
 
-					Section("Recent play sessions",
+					Section(
 						[
-							DataTable(recentGamePlayActionSessions.map(
-								(gamePlayActionSession) =>
-								{
-									let title = "Played for " + Utilities.NumberLib.format(gamePlayActionSession.playTimeSeconds) + " seconds on " + gamePlayActionSession.platform.name;
+							Header("Links"),
 
-									if (gamePlayActionSession.notes != null)
-									{
-										title += "\n\n" + gamePlayActionSession.notes;
-									}
-
-									if (configuration.developerMode)
-									{
-										title += "\n\nGamePlayActionSession #" + gamePlayActionSession.id;
-									}
-
-									return {
-										label: !gamePlayActionSession.isHistorical
-											? HumanDateTime(DateTime.fromJSDate(gamePlayActionSession.startDate))
-											: "Historical",
-										value: new DE("span",
-											{
-												title,
-											},
-											[
-												new DE("span", gamePlayActionSession.platform.iconName),
-												" ",
-												humanizeDuration(gamePlayActionSession.playTimeSeconds * 1000, { units: [ "h", "m", "s" ] }),
-											]),
-									};
-								})),
+							links.map((link) => Paragraph(Anchor(link.title, link.url, "_blank"))),
 						]),
 
-					Section("Links",
-						[
-							links.map((link) => new DE("p", null, Anchor(link.title, link.url, "_blank"))),
-						]),
+					game.notes != null
+						? Section(
+							[
+								Header("Notes"),
+
+								// TODO: render as markdown!
+								game.notes,
+							])
+						: null,
 				]),
 		]);
 }
