d972a2b20866259d67cacf5891ca9c03333cb3c0
Loren Goodwin <18320375+duckdotapk@users.noreply.github.com>
2025-01-02 22:09:15 -0500

    fix: fixed getImageUrls/getImagePaths

--- src/libs/models/Game.ts
+++ src/libs/models/Game.ts
@@ -135,7 +135,7 @@ export function getImageUrls(game: Prisma.GameGetPayload<null>)
 	return {
 		banner: "/data/images/games/" + game.id + "/banner.jpg",
 		cover: "/data/images/games/" + game.id + "/cover.jpg",
-		icon: "/data/images/games/" + game.id + "/icon.png",
+		icon: "/data/images/games/" + game.id + "/icon.jpg",
 		logo: "/data/images/games/" + game.id + "/logo.png",
 	};
 }
@@ -145,7 +145,7 @@ export function getImagePaths(game: Prisma.GameGetPayload<null>)
 	return {
 		banner: path.join(LGGL_DATA_DIRECTORY, "images", "games", game.id.toString(), "banner.jpg"),
 		cover: path.join(LGGL_DATA_DIRECTORY, "images", "games", game.id.toString(), "cover.jpg"),
-		icon: path.join(LGGL_DATA_DIRECTORY, "images", "games", game.id.toString(), "icon.png"),
+		icon: path.join(LGGL_DATA_DIRECTORY, "images", "games", game.id.toString(), "icon.jpg"),
 		logo: path.join(LGGL_DATA_DIRECTORY, "images", "games", game.id.toString(), "logo.png"),
 	}
 }
