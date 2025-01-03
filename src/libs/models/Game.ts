9bd3d1f23e1696f0857b950f5456082426e2f11a
Loren Goodwin <18320375+duckdotapk@users.noreply.github.com>
2025-01-03 12:56:51 -0500

    refactor: removed deprecated constants

--- src/libs/models/Game.ts
+++ src/libs/models/Game.ts
@@ -74,59 +74,6 @@ export type ModSupport = z.infer<typeof ModSupportSchema>;
 export type VirtualRealitySupport = z.infer<typeof VirtualRealitySupportSchema>;
 
 //
-// Constants
-//
-
-/** @deprecated */
-export const PROGRESSION_TYPE: Record<ProgressionType, string> =
-{
-	NONE: "NONE",
-	NON_CAMPAIGN: "NON_CAMPAIGN",
-	CAMPAIGN: "CAMPAIGN",
-};
-
-/** @deprecated */
-export const COMPLETION_STATUS: Record<CompletionStatus, string> =
-{
-	TODO: "TODO",
-	IN_PROGRESS: "IN_PROGRESS",
-	COMPLETE: "COMPLETE",
-	ONE_HUNDRED_PERCENT: "ONE_HUNDRED_PERCENT",
-};
-
-/** @deprecated */
-export const ACHIEVEMENT_SUPPORT: Record<AchievementSupport, string> =
-{
-	NONE: "NONE",
-	INGAME: "INGAME",
-	LAUNCHER: "LAUNCHER",
-};
-
-/** @deprecated */
-export const CONTROLLER_SUPPORT: Record<ControllerSupport, string> =
-{
-	NONE: "NONE",
-	SUPPORTED: "SUPPORTED",
-	REQUIRED: "REQUIRED",
-};
-
-/** @deprecated */
-export const MOD_SUPPORT: Record<ModSupport, string> =
-{
-	NONE: "NONE",
-	UNOFFICIAL: "UNOFFICIAL",
-	OFFICIAL: "OFFICIAL",
-};
-
-/** @deprecated */
-export const VIRTUAL_REALITY_SUPPORT: Record<VirtualRealitySupport, string> =
-{
-	NONE: "NONE",
-	SUPPORTED: "SUPPORTED",
-	REQUIRED: "REQUIRED",
-};
-
-//
 // Utility Functions
 //
 
