67c31f3f4880572a73478832842140805e8a1f5b
Loren Goodwin <18320375+duckdotapk@users.noreply.github.com>
2025-01-03 22:50:28 -0500

    feat: moved add-historical-playtime to the CLI

--- src/cli.ts
+++ src/cli.ts
@@ -33,6 +33,7 @@ const ActionNameSchema = z.enum(
 	[
 		"addGame",
 		"addGameInstallation",
+		"addHistoricalPlaytime",
 		"audit",
 	]);
 
@@ -509,6 +510,154 @@ async function addGameInstallation(readlineInterface: readline.promises.Interfac
 	await CliLib.pause(readlineInterface);
 }
 
+async function addHistoricalPlaytime(readlineInterface: readline.promises.Interface)
+{
+	const game = await searchForGame(readlineInterface);
+	
+	const gamePlayAction = await CliLib.prompt(readlineInterface, 
+		{
+			text: "Choose which game play action you want to associate this playtime with",
+			options: game.gamePlayActions.map(
+				(gamePlayAction) =>
+				{
+					return {
+						value: gamePlayAction.id.toString(),
+						description: gamePlayAction.name,
+					};
+				}),
+			validateAndTransform: async (input) =>
+			{
+				const inputParseResult = z.number().int().min(1).safeParse(parseInt(input.trim()));
+
+				if (!inputParseResult.success)
+				{
+					throw new CliLib.RetryableError("Invalid game play action ID.");
+				}
+
+				const id = inputParseResult.data;
+
+				const gamePlayAction = game.gamePlayActions.find((gamePlayAction) => gamePlayAction.id == id);
+
+				if (gamePlayAction == null)
+				{
+					throw new CliLib.RetryableError("No game play action found with that ID.");
+				}
+
+				return gamePlayAction;
+			},
+		});
+
+	const platforms = await prismaClient.platform.findMany(
+		{
+			orderBy:
+			{
+				id: "asc",
+			},
+		});
+
+	const platform = await CliLib.prompt(readlineInterface, 
+		{
+			text: "Enter the ID of the platform this playtime is from",
+			options: platforms.map(
+				(platform) =>
+				{
+					return {
+						value: platform.id.toString(),
+						description: platform.name,
+					};
+				}),
+			validateAndTransform: async (input) =>
+			{
+				const inputParseResult = z.number().int().min(1).safeParse(parseInt(input.trim()));
+
+				if (!inputParseResult.success)
+				{
+					throw new CliLib.RetryableError("Invalid platform ID.");
+				}
+
+				const id = inputParseResult.data;
+
+				const platform = platforms.find((platform) => platform.id == id);
+
+				if (platform == null)
+				{
+					throw new CliLib.RetryableError("No platform found with that ID.");
+				}
+
+				return platform;
+			},
+		});
+
+	const playTimeSeconds = await CliLib.prompt(readlineInterface, 
+		{
+			text: "Enter the playtime in HH:MM:SS format",
+			validateAndTransform: async (input) =>
+			{
+				const inputComponents = input.split(":").map((component) => parseInt(component));
+
+				const inputComponentsParseResult = z.tuple([ z.number().int(), z.number().int(), z.number().int() ]).safeParse(inputComponents);
+
+				if (!inputComponentsParseResult.success)
+				{
+					throw new CliLib.RetryableError("Invalid playtime format.");
+				}
+
+				const [ hours, minutes, seconds ] = inputComponentsParseResult.data;
+
+				return (hours * 60 * 60) + (minutes * 60) + seconds;
+			},
+		});
+
+	const notes = await CliLib.prompt(readlineInterface, 
+		{
+			text: "Enter any notes you want to add to this playtime",
+			validateAndTransform: async (input) =>
+			{
+				input = input.trim();
+
+				return input.length > 0 ? input : null;
+			},
+		});
+
+	readlineInterface.close();
+
+	const gamePlayActionSession = await prismaClient.$transaction(
+		async (transactionClient) =>
+		{
+			await transactionClient.game.update(
+				{
+					where:
+					{
+						id: game.id,
+					},
+					data:
+					{
+						playTimeTotalSeconds: { increment: playTimeSeconds },
+					},
+				});
+
+			return await transactionClient.gamePlayActionSession.create(
+				{
+					data:
+					{
+						startDate: DateTime.fromSeconds(0).toJSDate(),
+						endDate: DateTime.fromSeconds(playTimeSeconds).toJSDate(),
+						playTimeSeconds,
+						addedToTotal: true,
+						isHistorical: true,
+						notes,
+		
+						gamePlayAction_id: gamePlayAction.id,
+						platform_id: platform.id,
+					},
+				});
+		});
+
+	console.log("Created historical game play action session with ID: %d", gamePlayActionSession.id);
+
+	await CliLib.pause(readlineInterface);
+}
+
 async function audit(readlineInterface: readline.promises.Interface)
 {
 	const shouldFixData = await CliLib.confirm(readlineInterface,
@@ -910,6 +1059,11 @@ const actions: Record<ActionName, Action> =
 	{
 		description: "Add a new game installation to an existing game",
 		execute: async (readlineInterface) => addGameInstallation(readlineInterface),
+	},
+	addHistoricalPlaytime:
+	{
+		description: "Add historical playtime to an existing game",
+		execute: async (readlineInterface) => addHistoricalPlaytime(readlineInterface),
 	},
 	audit:
 	{
