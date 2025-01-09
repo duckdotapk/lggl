66250f226f3aca3caed0ebd64a34be44fdd64b8e
Loren Goodwin <18320375+duckdotapk@users.noreply.github.com>
2025-01-09 17:09:08 -0500

    feat: added /api/gameCompanies/update endpoint

---
+++ src/routes/api/gameCompany/create.schemas.ts
@@ -0,0 +1,58 @@
+//
+// Imports
+//
+
+import * as FritterApiUtilities from "@donutteam/fritter-api-utilities";
+import { z } from "zod";
+
+import * as GameCompanySchemaLib from "../../../libs/schemas/GameCompany.js";
+
+//
+// Schemas
+//
+
+export const RequestBodySchema = z.object(
+	{
+		notes: z.string().nullable(),
+		type: GameCompanySchemaLib.TypeSchema,
+
+		company_id: z.number().int(),
+		game_id: z.number().int(),
+	});
+
+export const ResponseBodySchema = z.union(
+	[
+		FritterApiUtilities.SuccessResponseBodySchema,
+
+		FritterApiUtilities.ErrorResponseBodySchema,
+	]);
+
+//
+// Types
+//
+
+export type RequestBody = z.infer<typeof RequestBodySchema>;
+	
+export type ResponseBody = z.infer<typeof ResponseBodySchema>;
+
+//
+// Constants
+//
+
+export const method = "POST";
+
+export const path = "/api/gameCompanies/create";
+
+//
+// Utility Functions
+//
+
+export function updateGameCompany(requestBody: RequestBody)
+{
+	return FritterApiUtilities.request(method, path,
+		{
+			requestBodySchema: RequestBodySchema,
+			responseBodySchema: ResponseBodySchema,
+			requestBody,
+		});
+}
---
+++ src/routes/api/gameCompany/create.ts
@@ -0,0 +1,69 @@
+//
+// Imports
+//
+
+import * as FritterApiUtilities from "@donutteam/fritter-api-utilities";
+
+import { prismaClient } from "../../../instances/prismaClient.js";
+import { ServerFritterContext } from "../../../instances/server.js";
+
+import * as Schemas from "./create.schemas.js";
+
+//
+// Route
+//
+
+type RouteFritterContext = ServerFritterContext;
+
+export const route = FritterApiUtilities.createEndpointRoute<RouteFritterContext, typeof Schemas.RequestBodySchema, typeof Schemas.ResponseBodySchema>(
+	{
+		method: Schemas.method,
+		path: Schemas.path,
+		middlewares: [],
+		requestBodySchema: Schemas.RequestBodySchema,
+		responseBodySchema: Schemas.ResponseBodySchema,
+		handler: async (requestBody) =>
+		{
+			const game = await prismaClient.game.findUnique(
+				{
+					where:
+					{
+						id: requestBody.game_id,
+					},
+				});
+
+			if (game == null)
+			{
+				throw new FritterApiUtilities.APIError({ code: "NOT_FOUND", message: "Game not found." });
+			}
+
+			const company = await prismaClient.company.findUnique(
+				{
+					where:
+					{
+						id: requestBody.company_id,
+					},
+				});
+
+			if (company == null)
+			{
+				throw new FritterApiUtilities.APIError({ code: "NOT_FOUND", message: "Company not found." });
+			}
+
+			await prismaClient.gameCompany.create(
+				{
+					data:
+					{
+						notes: requestBody.notes,
+						type: requestBody.type,
+
+						company_id: company.id,
+						game_id: game.id,
+					},
+				});
+
+			return {
+				success: true,
+			};
+		},
+	});
