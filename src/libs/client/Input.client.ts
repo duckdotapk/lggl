8adc5f306c590b51d52ce944cc83e16f748dc115
Loren Goodwin <18320375+duckdotapk@users.noreply.github.com>
2025-01-09 18:10:02 -0500

    refactor: refactored InputClientLib, again

--- src/libs/client/Input.client.ts
+++ src/libs/client/Input.client.ts
@@ -9,89 +9,139 @@ import { ZodTypeAny } from "zod";
 // Utility Functions
 //
 
+export function getBooleanValue(input: HTMLInputElement)
+{
+	return input.checked;
+}
+
+export function getDateValue(input: HTMLInputElement)
+{
+	return input.value.trim();
+}
+
+export function getDateValueNullable(input: HTMLInputElement)
+{
+	return input.value.trim() != "" ? input.value.trim() : null;
+}
+
+export function getDateTimeValue(input: HTMLInputElement)
+{
+	return input.value.trim();
+}
+
+export function getDateTimeValueNullable(input: HTMLInputElement)
+{
+	return input.value.trim() != "" ? input.value.trim() : null;
+}
+
+export function getNumberValue(input: HTMLInputElement)
+{
+	return Number(input.value);
+}
+
+export function getNumberValueNullable(input: HTMLInputElement)
+{
+	return input.value.trim() != "" ? Number(input.value) : null;
+}
+
+export function getEnumValue<T extends ZodTypeAny>(input: HTMLSelectElement, schema: T)
+{
+	return schema.parse(input.value);
+}
+
+export function getEnumValueNullable<T extends ZodTypeAny>(input: HTMLSelectElement, schema: T)
+{
+	return input.value.trim() != "" ? schema.parse(input.value.trim()) : null;
+}
+
+export function getStringValue(input: HTMLInputElement | HTMLSelectElement | HTMLTextAreaElement)
+{
+	return input.value;
+}
+
 export function isInputDirty(input: HTMLInputElement | HTMLSelectElement | HTMLTextAreaElement)
 {
 	return BrowserUtilities.ElementClientLib.getBooleanData(input, "dirty") ?? false;
 }
 
-export function getBooleanValue(input: HTMLInputElement)
+export function getChangedBooleanValue(input: HTMLInputElement)
 {
 	return isInputDirty(input) ? input.checked : undefined;
 }
 
-export function getDateValue(input: HTMLInputElement)
+export function getChangedDateValue(input: HTMLInputElement)
 {
-	return isInputDirty(input) ? input.value : undefined;
+	return isInputDirty(input) ? input.value.trim() : undefined;
 }
 
-export function getDateValueNullable(input: HTMLInputElement)
-{
-	if (!isInputDirty(input))
-	{
-		return undefined;
-	}
-
-	return input.value != "" ? input.value : null;
-}
-
-export function getDateTimeValue(input: HTMLInputElement)
-{
-	return isInputDirty(input) ? input.value : undefined;
-}
-
-export function getDateTimeValueNullable(input: HTMLInputElement)
+export function getChangedDateValueNullable(input: HTMLInputElement)
 {
 	if (!isInputDirty(input))
 	{
 		return undefined;
 	}
 
-	return input.value.trim().length > 0 ? input.value : null;
+	return input.value.trim() != "" ? input.value.trim() : null;
 }
 
-export function getEnumValue<T extends ZodTypeAny>(input: HTMLSelectElement, schema: T)
+export function getChangedDateTimeValue(input: HTMLInputElement)
 {
-	return isInputDirty(input) ? schema.parse(input.value) : undefined;
+	return isInputDirty(input) ? input.value.trim() : undefined;
 }
 
-export function getEnumValueNullable<T extends ZodTypeAny>(input: HTMLSelectElement, schema: T)
+export function getChangedDateTimeValueNullable(input: HTMLInputElement)
 {
 	if (!isInputDirty(input))
 	{
 		return undefined;
 	}
 
-	return input.value != "" ? schema.parse(input.value) : null;
+	return input.value.trim() != "" ? input.value.trim() : null;
 }
 
-export function getNumberValue(input: HTMLInputElement)
+export function getChangedEnumValue<T extends ZodTypeAny>(input: HTMLSelectElement, schema: T)
+{
+	return isInputDirty(input) ? schema.parse(input.value.trim()) : undefined;
+}
+
+export function getChangedEnumValueNullable<T extends ZodTypeAny>(input: HTMLSelectElement, schema: T)
+{
+	if (!isInputDirty(input))
+	{
+		return undefined;
+	}
+
+	return input.value.trim() != "" ? schema.parse(input.value.trim()) : null;
+}
+
+export function getChangedNumberValue(input: HTMLInputElement)
 {
 	return isInputDirty(input) ? Number(input.value) : undefined;
 }
 
-export function getNumberValueNullable(input: HTMLInputElement)
+export function getChangedNumberValueNullable(input: HTMLInputElement)
 {
 	if (!isInputDirty(input))
 	{
 		return undefined;
 	}
 
-	return input.value.trim() == "" ? null : Number(input.value);
+	return input.value.trim() != "" ? Number(input.value) : null;
 }
 
-export function getStringValue(input: HTMLInputElement | HTMLSelectElement | HTMLTextAreaElement)
+export function getChangedStringValue(input: HTMLInputElement | HTMLSelectElement | HTMLTextAreaElement)
 {
-	return isInputDirty(input) ? input.value : undefined;
+	return isInputDirty(input) ? input.value.trim() : undefined;
 }
 
-export function getStringValueNullable(input: HTMLInputElement | HTMLSelectElement | HTMLTextAreaElement)
+export function getChangedStringValueNullable(input: HTMLInputElement | HTMLSelectElement | HTMLTextAreaElement)
 {
 	if (!isInputDirty(input))
 	{
 		return undefined;
 	}
 
-	return input.value.trim().length > 0 ? input.value : null;
+	return input.value.trim() != "" ? input.value.trim() : null;
 }
 
 export function clearDirtyInputs(form: HTMLFormElement)
