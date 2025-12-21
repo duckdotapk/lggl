42bb0e78418476147d2fa1a04dfde32b427b1940
Loren Goodwin <18320375+duckdotapk@users.noreply.github.com>
2025-12-21 13:02:52 -0500

    refactor: explicitly pass LGGL_DATABASE_URL to PrismaClient

--- src/instances/prismaClient.ts
+++ src/instances/prismaClient.ts
@@ -4,8 +4,13 @@
 
 import { PrismaClient } from "@prisma/client";
 
+import { LGGL_DATABASE_URL } from "../env/LGGL_DATABASE_URL.js";
+
 //
 // Exports
 //
 
-export const prismaClient = new PrismaClient();
+export const prismaClient = new PrismaClient(
+{
+	datasourceUrl: LGGL_DATABASE_URL,
+});
